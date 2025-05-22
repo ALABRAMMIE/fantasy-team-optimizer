e23cf3841bb26d5f0516440ac0168e2307de0c4e
ALABRAMMIE <br.seghers@gmail.com>
2025-05-22 09:36:51 +0200

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ sport_options = [
 
 
 sport = st.sidebar.selectbox("Select a sport", sport_options)
-template_file = st.sidebar.file_uploader("Upload Target Profile Template (multi-sheet)", type=["xlsx"], key="template")
+template_file = st.sidebar.file_uploader("Upload Target Profile Template (multi-sheet)", type=["xlsx"], key="template_file_uploader")
 
 available_formats = []
 format_name = None
@@ -47,7 +47,7 @@ solver_mode = st.sidebar.radio("Solver Objective", [
 ])
 
 uploaded_file = st.file_uploader("Upload your Excel file (players)", type=["xlsx"])
-template_file = st.sidebar.file_uploader("Upload Target Profile Template (multi-sheet)", type=["xlsx"], key="template")
+template_file = st.sidebar.file_uploader("Upload Target Profile Template (multi-sheet)", type=["xlsx"], key="template_file_uploader")
 
 if uploaded_file:
     df = pd.read_excel(uploaded_file)
