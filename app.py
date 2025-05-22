e58b9b438dc3d0a15cdeb181feebdab4804be74f
ALABRAMMIE <br.seghers@gmail.com>
2025-05-22 10:52:04 +0200

    Update app.py

--- app.py
+++ app.py
@@ -121,7 +121,7 @@ if uploaded_file:
 
         
 if optimize_clicked:
-    st.info("🟡 Optimize button clicked.")
+        st.info("🟡 Optimize button clicked.")
     if solver_mode == "Closest FTP Match" and not target_values:
         st.warning("⚠️ Target values are not loaded. Please check your format sheet or template.")
     elif solver_mode == "Closest FTP Match" and bracket_constraint_failed:
